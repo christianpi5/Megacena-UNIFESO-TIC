1932412435029c11111c2ccf3eaceafab967f710
Mat_Andre <andrellucas093@gmail.com>
2025-05-26 22:03:16 -0300

    Alterado o método para exibição do menu
    - Adicionado arquivos .exe e .gch para o gitignore
    
    - Menu agora é exibido por apenas um loop, sem a necessidade de salvar os números em array

--- megasena.c
+++ megasena.c
@@ -4,36 +4,21 @@
 
 void menu()
 {
-    int contador = 0;
-
-    printf("\n\t\t*** MEGA SENA ***\n");
-
-    int* array_de_numeros_para_serem_exibidos = malloc(60 * sizeof(int));
-    //Cria aqui um array com 60 numeros
-    
-    for(int i  = 0; i < 60; i++){
-        array_de_numeros_para_serem_exibidos[i] = i + 1;
-    }
+    printf("\n\t\t** MEGA SENA **\n");
 
     for(int j = 0; j < 60; j++){
-
-        if(contador == 10){ //pula linha de 10 em 10 pra sair bonitinho no terminal xD
+		// Toda vez que o j for divisivel por 10 ele da a quebra de linha
+        if(j % 10 == 0){
             printf("\n");
-            contador = 0;
         }
-        contador++;
-        
-        if(array_de_numeros_para_serem_exibidos[j] < 9){
-            printf("[ 0%d ]", array_de_numeros_para_serem_exibidos[j]); //Acrescenta um zero a esquerda para numeros < 9 para deixar a formatação mais bonita
+        //Assim tira a necessidade de ter 2 dois for, e reduz pra um só, sem ter a necessidade de salvar os números a serem exibidos
+        if(j + 1 < 10 ){
+            printf("[ 0%d ]", j + 1); //Acrescenta um zero a esquerda para numeros < 9 para deixar a formatação mais bonita
         }else{
-            printf("[ %d ]", array_de_numeros_para_serem_exibidos[j]); 
+            printf("[ %d ]", j + 1); 
         }
-        
     }
-
-    free(array_de_numeros_para_serem_exibidos); //liberar pra nao dar ruim
 }
-
 void solicitacoes(int* quantidade, char *frase,  int condicao1, int condicao2) //modularizar solicitações
 {
     printf("\n\n %s \n", frase);
