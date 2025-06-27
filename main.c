e89738f101a01237298c33ee015753d5e497f01b
Mat_Andre <60665937+Andre-Lucas-093@users.noreply.github.com>
2025-06-27 20:18:03 -0300

    Criado o valor da aposta do usuário e quantidade de teimosinhas
    - Agora um cálculo é feito para saber quanto o usuário vai ter que pagar na sua aposta
    
    - Criado um input pro usuário inserir a quantidade de teimosinhas que ele gostaria

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include "megasena.h"
-#include "megasena.c"
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
 int main(){
     int quantidade_de_dezenas_por_aposta; //dezenas escolhidas pelo usuario
     int quantidade_de_apostas_manuais;  //apostas escolhidas pelo usuario
     int teimosinhas, quantidade_de_surpresinhas;
-    
+    float valor_aposta;
+    float valor_dezena[] = {5.00,35.00,140.00,420.00,1050.00,2310.00,4620.00,8580.00,15015.00,25025.00};
     int ** dezenas_escolhidas; //Matriz pra saber as dezenas escolhidas
     int ** surpresinhas_sorteadas; //Matriz para salvar as surpresinhas do usuário
 	int *dezenas_sorteadas; // Vetor pra saber qual vai ser a combinação vencedora
 	
-	//Variável para controle de qual opção do menu o usuário vai escolher sendo 4 = saída
 	menu();
 
 
@@ -30,12 +30,24 @@ int main(){
 	
 	//Agora a solicitação pela quantidade de surpresinhas é feita pela função de solicitação
 	solicitacoes(&quantidade_de_surpresinhas, "Digite a quantidade de surpresinhas que voce quer", 0, 7);
-	
+	while (true){
+	    printf("\nDigite quantas teimosinhas voce gostaria: ");
+	    scanf("%d",&teimosinhas);
+	    
+	    if(teimosinhas == 0 || teimosinhas == 2 || teimosinhas == 4 || teimosinhas == 8){
+	        break;
+	    }
+	    printf("Valor invalido, voce pode escolher, 0, 2 ,4 ou 8 surpresinhas\n");
+	}
 	surpresinhas_sorteadas = (int**)malloc(quantidade_de_surpresinhas * sizeof(int*));
 	for(int c = 0; c < quantidade_de_surpresinhas; c++){
 		surpresinhas_sorteadas[c] = (int*)malloc(quantidade_de_dezenas_por_aposta * sizeof(int));
 	}
 	gerar_matriz_pai_de_surpresinhas(quantidade_de_surpresinhas,quantidade_de_dezenas_por_aposta,surpresinhas_sorteadas);
+    valor_aposta = teimosinhas * (valor_dezena[quantidade_de_dezenas_por_aposta - 6] * (quantidade_de_apostas_manuais + quantidade_de_surpresinhas));
+    
+    printf("A sua aposta requer um deposito de: %.2f\nDeseja confirmar? ",valor_aposta);
+    
 //				Essa parte abaixo eu coloquei só pra mostrar que agora a main tem acesso as dezenas escolhidas pelo usuário
 //				Esse mesmo código funciona se refeito para surpresinhas   			
 //				for(int i = 0; i < quantidade_de_apostas_manuais ; i++){
